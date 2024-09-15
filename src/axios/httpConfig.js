10e67286b40ed5f230d4221583e9c29893cb90be
sweonurulu <73286934+sweonurulu@users.noreply.github.com>
2024-09-15 15:18:27 +0300

    Update httpConfig.js

--- src/axios/httpConfig.js
+++ src/axios/httpConfig.js
@@ -3,7 +3,7 @@ import axios from "axios";
 import Cookies from 'js-cookie'; // veya token'ı sakladığınız başka bir yer
 
 const HTTP = axios.create({
-  baseURL: "https://ly-backend-8gjq.onrender.com",
+  baseURL: "https://ly-backend-8gjq.onrender.com:5000",
   headers: {
     'Content-Type': 'application/json',
 },

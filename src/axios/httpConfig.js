ab37919712c5407524a2ec090888211df02110c8
sweonurulu <73286934+sweonurulu@users.noreply.github.com>
2024-09-24 01:43:27 +0300

    Add files via upload

---
+++ src/axios/httpConfig.js
@@ -0,0 +1,30 @@
+// axios/httpConfig.js
+import axios from "axios";
+import Cookies from 'js-cookie'; // veya token'ı sakladığınız başka bir yer
+
+const HTTP = axios.create({
+  baseURL: "http://localhost:5000", 
+  headers: {
+    'Content-Type': 'application/json',
+},
+});
+
+  // https://ly-backend-8gjq.onrender.com
+  // http://localhost:5000
+  // onur ulu
+
+// Token'ı her istekle birlikte eklemek için interceptors kullanın
+HTTP.interceptors.request.use(
+(config) => {
+    const token = Cookies.get('token'); // Veya token'ı nerede saklıyorsanız
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+},
+(error) => {
+    return Promise.reject(error);
+}
+);
+
+export default HTTP;
